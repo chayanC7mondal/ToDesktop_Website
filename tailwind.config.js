fdf906fc0d84865e1b1b39103f85b1724333b6d7
Chayan Mondal <mitalichayan18082004@gmail.com>
2024-05-14 11:06:20 +0530

    new project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+colors:{
+  'primary': '#3490dc',
+},
+fontFamily:{
+  'chayan_font':['']
+}
+
+    },
+  },
+  plugins: [],
+}
+
